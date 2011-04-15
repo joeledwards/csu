9f9fbc4c5b7f9fce0aa89da06b3bb5bdd0c33fc2
Joel Edwards <joeledwards@gmail.com>
2011-04-15 00:11:04 -0600

    Updated Homework 5

--- java1/hw4/DrawBoard.java
+++ java1/hw4/DrawBoard.java
@@ -1,10 +1,11 @@
 import java.applet.Applet;
+import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.event.ComponentListener;
-import java.awt.event.ComponentEvent;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.ComponentListener;
+import java.awt.event.ComponentEvent;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import javax.swing.JApplet;
