44aa6788c7c31c6252a3083694b7a5e3dabe5521
Joel Edwards <joeledwards@gmail.com>
2011-05-29 22:14:34 -0600

    Completed editor project.

--- java1/hw9/editor/src/Editor.java
+++ java1/hw9/editor/src/Editor.java
@@ -3,31 +3,42 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+import javax.swing.UIManager;
 
 public class Editor
 extends JFrame
-implements  ActionListener
+implements ActionListener, ChangeListener, KeyListener
 {
     public static void main(String[] args)
     {
         Editor window = new Editor("Editor");
-        window.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        window.addWindowListener(new Terminator());
+        window.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         window.setVisible(true);
     }
 
  // Class Components
+    private String clipboard = null;
     private String title;
+    private File defaultOpenDir = new File(".");
+    private File defaultSaveDir = new File(".");
+    private Terminator terminator = null;
 
     private int currentTab = 0;
     private boolean isDoubleBuffered = false;
@@ -37,12 +48,25 @@ implements  ActionListener
     private JPanel buttonPanel = new JPanel(new BorderLayout());
     private JButton loadButton = new JButton("Load");
     private JButton saveButton = new JButton("Save");
-    private JMenuBar menuBar = null;
 
-    private Confirm confirmQuit = new Confirm("Tab Contents Modified",
-                                              "This tab has been modified. Do you wish to close without saving?");
-    private Confirm confirmClose = new Confirm("Confirm Quit",
-                                               "There are tabs with modified content. Do you wish to quit?");
+    private JMenuBar menuBar = new JMenuBar();
+    private JMenu fileMenu   = new JMenu("File");
+    private JMenu editMenu   = new JMenu("Edit");
+
+ // File Menu Items
+    private JMenuItem newFileMenuItem    = new JMenuItem("New",     KeyEvent.VK_N);
+    private JMenuItem openFileMenuItem   = new JMenuItem("Open",    KeyEvent.VK_O);
+    private JMenuItem saveFileMenuItem   = new JMenuItem("Save",    KeyEvent.VK_S);
+    private JMenuItem closeFileMenuItem  = new JMenuItem("Close",   KeyEvent.VK_C);
+    private JMenuItem saveAsFileMenuItem = new JMenuItem("Save As", KeyEvent.VK_A);
+    private JMenuItem quitFileMenuItem   = new JMenuItem("Quit",    KeyEvent.VK_Q);
+
+ // Edit Menu Items
+    private JMenuItem selectAllEditMenuItem = new JMenuItem("Select All", KeyEvent.VK_A);
+    private JMenuItem copyEditMenuItem   = new JMenuItem("Copy",  KeyEvent.VK_C);
+    private JMenuItem cutEditMenuItem    = new JMenuItem("Cut",   KeyEvent.VK_X);
+    private JMenuItem pasteEditMenuItem  = new JMenuItem("Paste", KeyEvent.VK_P);
+
 
     public Editor(String title)
     {
@@ -52,7 +76,7 @@ implements  ActionListener
 
         tabs.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
 
-        menuBar = createMenu();
+        createMenu();
         setJMenuBar(menuBar);
     
         setLayout(new BorderLayout());
@@ -60,47 +84,59 @@ implements  ActionListener
         buttonPanel.add(saveButton, BorderLayout.EAST);
         add(buttonPanel, BorderLayout.SOUTH);
         add(tabs, BorderLayout.CENTER);
-        newTab(null);
+        createNewTab(null);
         pack();
 
+        terminator = new Terminator(this, tabs);
+        this.addWindowListener(terminator);
         loadButton.addActionListener(this);
-        saveButton.addActionListener(this); }
+        saveButton.addActionListener(this); 
 
-    private JMenuBar createMenu()
-    {
-        JMenuBar menuBar = new JMenuBar();
+     // Make sure keyboard shortcut combinations work everywhere
+        this.addKeyListener(this);
+        this.getContentPane().addKeyListener(this);
+        tabs.addKeyListener(this);
+        loadButton.addKeyListener(this);
+        saveButton.addKeyListener(this); 
 
-     // File Menu
-        JMenu fileMenu = new JMenu("File");
-        fileMenu.setMnemonic(KeyEvent.VK_F);
-        menuBar.add(fileMenu);
-
-        JMenuItem openFile = new JMenuItem("Open", KeyEvent.VK_O);
-        fileMenu.add(openFile);
-
-        JMenuItem saveFile = new JMenuItem("Save", KeyEvent.VK_S);
-        fileMenu.add(saveFile);
-
-        JMenuItem saveAsFile = new JMenuItem("Save As", KeyEvent.VK_A);
-        fileMenu.add(saveAsFile);
-
-     // Tab Menu
-        JMenu tabMenu = new JMenu("Tab");
-        tabMenu.setMnemonic(KeyEvent.VK_T);
-        menuBar.add(tabMenu);
-
-        JMenuItem newTab = new JMenuItem("New", KeyEvent.VK_N);
-        tabMenu.add(newTab);
-
-        JMenuItem closeTab = new JMenuItem("Close", KeyEvent.VK_C);
-        tabMenu.add(closeTab);
-
-        return menuBar;
+        updateGui();
     }
 
-    private void newTab(File file)
+    private void createMenu()
     {
-        Tab tab = new Tab(tabs, file, isDoubleBuffered);
+     // File Menu
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+        menuBar.add(fileMenu);
+        fileMenu.add(newFileMenuItem);
+        fileMenu.add(openFileMenuItem);
+        fileMenu.add(saveFileMenuItem);
+        fileMenu.add(saveAsFileMenuItem);
+        fileMenu.add(closeFileMenuItem);
+        fileMenu.add(quitFileMenuItem);
+        newFileMenuItem.addActionListener(this);
+        openFileMenuItem.addActionListener(this);
+        saveFileMenuItem.addActionListener(this);
+        saveAsFileMenuItem.addActionListener(this);
+        closeFileMenuItem.addActionListener(this);
+        quitFileMenuItem.addActionListener(this);
+
+     // Edit Menu
+        editMenu.setMnemonic(KeyEvent.VK_E);
+        menuBar.add(editMenu);
+        editMenu.add(selectAllEditMenuItem);
+        editMenu.add(copyEditMenuItem);
+        editMenu.add(cutEditMenuItem);
+        editMenu.add(pasteEditMenuItem);
+        selectAllEditMenuItem.addActionListener(this);
+        copyEditMenuItem.addActionListener(this);
+        cutEditMenuItem.addActionListener(this);
+        pasteEditMenuItem.addActionListener(this);
+    }
+
+ // Tab Operations
+    private void createNewTab(File file)
+    {
+        Tab tab = new Tab(this, tabs, file, isDoubleBuffered);
         tabs.setSelectedIndex(tabs.indexOfComponent(tab));
     }
 
@@ -111,58 +147,289 @@ implements  ActionListener
         }
     }
 
-    private void closeTab()
+    private void closeCurrentTab()
     {
-        if (tabs.getTabCount() >= 1) {
-            tabs.removeTabAt(tabs.getSelectedIndex());
+        int index = tabs.getSelectedIndex();
+        if (index >= 0) {
+            Tab tab = (Tab)tabs.getComponentAt(index);
+            tab.close();
         }
     }
 
-    private void open(File file)
+    private int getOpenIndex(File file) {
+        int total = tabs.getTabCount();
+        Tab tab = null;
+        for (int i = 0; i < total; i++) {
+            tab = (Tab)tabs.getComponentAt(i);
+            if ((tab.getFile() != null) && tab.getFile().equals(file)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+ // File Operations
+    private void open()
     {
-        Tab tab = (Tab)(tabs.getSelectedComponent());
-        if (tab.isFresh()) {
-            tab.setFile(file);
-        } else {
-            newTab(file);
+        // select file to load
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Open File");
+        fileChooser.setCurrentDirectory(defaultOpenDir);
+        fileChooser.setMultiSelectionEnabled(false);
+        int returnVal = fileChooser.showOpenDialog(this);
+        if (returnVal == JFileChooser.APPROVE_OPTION)
+        {
+            defaultOpenDir = fileChooser.getCurrentDirectory();
+            File file = fileChooser.getSelectedFile();
+
+            int openIndex = getOpenIndex(file);
+            if (openIndex < 0) {
+                Tab tab = (Tab)(tabs.getSelectedComponent());
+                if ((tab != null) && tab.isFresh()) {
+                    tab.setFile(file);
+                } else {
+                    createNewTab(file);
+                }
+            } else {
+                tabs.setSelectedIndex(openIndex);
+            }
         }
     }
 
     private void save()
     {
+        int index = tabs.getSelectedIndex();
+        if (index < 0) {
+            return;
+        }
+
+        Tab tab = (Tab)tabs.getComponentAt(index);
+        if (tab.getFile() == null) {
+            saveAs();
+        } else {
+            tab.save();
+        }
     }
 
     private void saveAs()
     {
+        int index = tabs.getSelectedIndex();
+        if (index < 0) {
+            return;
+        }
+
+        Tab tab = (Tab)tabs.getComponentAt(index);
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Save As File");
+        fileChooser.setCurrentDirectory(defaultSaveDir);
+        fileChooser.setMultiSelectionEnabled(false);
+        int returnVal = fileChooser.showSaveDialog(this);
+        if (returnVal == JFileChooser.APPROVE_OPTION)
+        {
+            defaultSaveDir = fileChooser.getCurrentDirectory();
+            File file = fileChooser.getSelectedFile();
+            tab.saveToFile(file);
+        }
     }
 
+ // Edit Operations
+    private void selectAll()
+    {
+        int index = tabs.getSelectedIndex();
+        if (index < 0) {
+            return;
+        }
+        Tab tab = (Tab)tabs.getComponentAt(index);
+        tab.getArea().selectAll();
+    }
 
- // Operations:
- // - save
- // - save as
- // - open
- // - new tab
+    private static int EDIT_OPERATION_COPY  = 1;
+    private static int EDIT_OPERATION_CUT   = 2;
+    private static int EDIT_OPERATION_PASTE = 3;
 
- // Behavior:
- // - if tab is selected and fresh, open operation will take over this tab
- // - all other open operations create a new tab
- // - when opening a file, compare to those that are open. If it is one of those,
- //   give that file's tab focus
- // - when a tab gains focus, update the currentTab value
+    private void editOperation(int operation) {
+        int index = tabs.getSelectedIndex();
+        if (index >= 0) {
+            Tab tab = (Tab)tabs.getComponentAt(index);
+            JTextArea area = tab.getArea();
+            int selectionStart = area.getSelectionStart();
+            int selectionEnd = area.getSelectionEnd();
+            int selectionWidth = selectionEnd - selectionStart;
+            int caretPosition = area.getCaretPosition();
+            if (selectionWidth > 0) {
+                if ((operation == EDIT_OPERATION_COPY) ||
+                    (operation == EDIT_OPERATION_CUT)) {
+                    clipboard = area.getSelectedText();
+                    if (operation == EDIT_OPERATION_CUT) {
+                        area.replaceSelection("");
+                    }
+                } else if (operation == EDIT_OPERATION_PASTE) {
+                    area.replaceSelection(clipboard);
+                }
+            } else if ((operation == EDIT_OPERATION_PASTE) &&
+                       (caretPosition >= 0)) {
+                area.insert(clipboard, caretPosition);
+            }
+        }
+    }
 
- // Delegate Methods
+    private void copy()
+    {
+        editOperation(EDIT_OPERATION_COPY);
+    }
+
+    private void cut()
+    {
+        editOperation(EDIT_OPERATION_CUT);
+    }
+
+    private void paste()
+    {
+        editOperation(EDIT_OPERATION_PASTE);
+    }
+
+ // Quit the Application
+    private void quit() {
+        terminator.windowClosing(null);
+    }
+
+ // Keep the GUI up-to-date after various events
+    public void updateGui()
+    {
+        int index = tabs.getSelectedIndex();
+        if (index >= 0) {
+            Tab tab = (Tab)tabs.getComponentAt(index);
+            if (tab.isFresh()) {
+                saveFileMenuItem.setEnabled(false);
+                saveAsFileMenuItem.setEnabled(false);
+            } else {
+                saveAsFileMenuItem.setEnabled(true);
+                if (tab.isSaved()) {
+                    saveFileMenuItem.setEnabled(false);
+                } else {
+                    saveFileMenuItem.setEnabled(true);
+                }
+            }
+            closeFileMenuItem.setEnabled(true);
+
+            JTextArea area = tab.getArea();
+            int selectionStart = area.getSelectionStart();
+            int selectionEnd = area.getSelectionEnd();
+            int selectionWidth = selectionEnd - selectionStart;
+            if (selectionWidth > 0) {
+                cutEditMenuItem.setEnabled(true);
+                copyEditMenuItem.setEnabled(true);
+            } else {
+                cutEditMenuItem.setEnabled(false);
+                copyEditMenuItem.setEnabled(false);
+            }
+
+            int caretPosition = area.getCaretPosition();
+            if (((caretPosition >= 0) || (selectionWidth > 0)) && (clipboard != null)) {
+                pasteEditMenuItem.setEnabled(true);
+            } else {
+                pasteEditMenuItem.setEnabled(false);
+            }
+        } else {
+            closeFileMenuItem.setEnabled(false);
+            saveFileMenuItem.setEnabled(false);
+            saveAsFileMenuItem.setEnabled(false);
+        }
+    }
+
+ // ActionListener
     public void actionPerformed(ActionEvent evt)
     {
         Object source = evt.getSource();
         if (source == loadButton) {
-            newTab(null);
-            // load new file
-            // on cancel, keep old file open
-            // update
+            open();
         }
         else if (source == saveButton) {
-            // save file
-            // 
+            save();
         }
+        else if (source == openFileMenuItem) {
+            open();
+        }
+        else if (source == saveFileMenuItem) {
+            save();
+        }
+        else if (source == saveAsFileMenuItem) {
+            saveAs();
+        }
+        else if (source == newFileMenuItem) {
+            createNewTab(null);
+        }
+        else if (source == closeFileMenuItem) {
+            closeCurrentTab();
+        }
+        else if (source == quitFileMenuItem) {
+            quit();
+        }
+        else if (source == selectAllEditMenuItem) {
+            selectAll();
+        }
+        else if (source == copyEditMenuItem) {
+            copy();
+        }
+        else if (source == cutEditMenuItem) {
+            cut();
+        }
+        else if (source == pasteEditMenuItem) {
+            paste();
+        }
+        updateGui();
+    }
+
+ // StateListener
+    public void stateChanged(ChangeEvent evt) {
+        Object source = evt.getSource();
+        if (source == tabs) {
+            updateGui();
+        }
+    }
+
+ // KeyListener
+    public void keyPressed(KeyEvent evt) {
+        int modifiers = evt.getModifiersEx();
+        int keyCode = evt.getKeyCode();
+
+     // Ctrl + <key>
+        int onmask = KeyEvent.CTRL_DOWN_MASK;
+        int offmask = KeyEvent.SHIFT_DOWN_MASK | KeyEvent.ALT_DOWN_MASK;
+        if ((modifiers & (onmask | offmask)) == onmask) {
+            if (keyCode == KeyEvent.VK_S) {
+                save();
+            } else if (keyCode == KeyEvent.VK_A) {
+                selectAll();
+            } else if (keyCode == KeyEvent.VK_Q) {
+                quit();
+            } else if (keyCode == KeyEvent.VK_N) {
+                createNewTab(null);
+            } else if (keyCode == KeyEvent.VK_O) {
+                open();
+            } else if (keyCode == KeyEvent.VK_W) {
+                closeCurrentTab();
+            }
+        }
+
+     // Ctrl + Shift + <key>
+        onmask = KeyEvent.CTRL_DOWN_MASK | KeyEvent.SHIFT_DOWN_MASK;
+        offmask = KeyEvent.ALT_DOWN_MASK;
+        if ((modifiers & (onmask | offmask)) == onmask) {
+            if (keyCode == KeyEvent.VK_S) {
+                saveAs();
+            }
+        }
+
+        updateGui();
+    }
+
+    public void keyReleased(KeyEvent evt) {
+        updateGui();
+    }
+
+    public void keyTyped(KeyEvent evt) {
+        updateGui();
     }
 }
--- java1/hw9/editor/src/Tab.java
+++ java1/hw9/editor/src/Tab.java
@@ -1,62 +1,97 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
+import javax.swing.UIManager;
 
 public class Tab
 extends JPanel
-implements ChangeListener, ActionListener
+implements ActionListener, CaretListener, ChangeListener, DocumentListener
 {
-    private boolean fresh = true;
-    private boolean saved = true;
-    private File file = null;
-    private JTabbedPane parent = null;
-    private JPanel tabComponent = null;
-    private JButton closeTabButton = null;
-    private JTextArea area = new JTextArea();
-    private JScrollPane scrollArea = new JScrollPane(area);
-
-    private static ImageIcon closeTabIcon = Resources.getAsImageIcon("resources/icons/close.png", 10, 10);
+    private static ImageIcon closeTabIcon  = Resources.getAsImageIcon("resources/icons/close.png", 10, 10);
+    private static ImageIcon modifiedTabIcon = Resources.getAsImageIcon("resources/icons/modified.png", 16, 16);
+    private static ImageIcon freshTabIcon  = Resources.getAsImageIcon("resources/icons/fresh.png", 16, 16);
+    private static ImageIcon savedTabIcon  = Resources.getAsImageIcon("resources/icons/saved.png", 16, 16);
     private static Dimension closeTabButtonSize = new Dimension(closeTabIcon.getIconWidth() + 4, closeTabIcon.getIconHeight() + 4);
     private static int untitledCount = 0;
 
-    public Tab(JTabbedPane parent, File file, boolean isDoubleBuffered)
+
+    private boolean fresh = true;
+    private boolean saved = true;
+    private File file = null;
+    private Editor parent = null;
+    private JTabbedPane pane = null;
+    private JPanel tabComponent = null;
+    private JLabel tabIcon = null;// for icon FINISH!
+    private JLabel tabLabel = null;
+    private JButton tabButton = null;
+    private JTextArea area = new JTextArea();
+    private JScrollPane scrollArea = new JScrollPane(area);
+
+
+    public Tab(Editor parent, JTabbedPane pane, File file, boolean isDoubleBuffered)
     {
         super(new BorderLayout(), isDoubleBuffered);
         assert(parent != null);
         this.parent = parent;
-        this.file = file;
+        this.pane = pane;
         add(scrollArea, BorderLayout.CENTER);
 
         String title = "Untitled" + ++untitledCount;
-        if (file != null) {
-            title = file.getName();
-        }
-        int index = parent.getTabCount();
-        parent.insertTab(null, null, this, "", index);
+        int index = pane.getTabCount();
+        pane.insertTab(null, null, this, "", index);
 
         tabComponent = new JPanel(new BorderLayout());
-        closeTabButton = new JButton(closeTabIcon);
-        closeTabButton.setPreferredSize(closeTabButtonSize);
-        tabComponent.add(new JLabel(title), BorderLayout.WEST);
-        tabComponent.add(closeTabButton, BorderLayout.EAST);
-        parent.setTabComponentAt(index, tabComponent);
+        tabIcon = new JLabel(freshTabIcon);
+        tabLabel = new JLabel(title);
+        tabButton = new JButton(closeTabIcon);
+        tabButton.setPreferredSize(closeTabButtonSize);
 
-        closeTabButton.addActionListener(this);
+        tabComponent.add(tabIcon, BorderLayout.WEST);
+        tabComponent.add(tabLabel, BorderLayout.CENTER);
+        tabComponent.add(tabButton, BorderLayout.EAST);
+        pane.setTabComponentAt(index, tabComponent);
+
+        tabButton.addActionListener(this);
+        area.getDocument().addDocumentListener(this);
+        area.addCaretListener(this);
+        area.addKeyListener(parent);
 
         if (file != null) {
-            load();
+            setFile(file);
         }
+    }
+
+    private void setSaved(boolean saved) {
+        this.fresh = false;
+        this.saved = saved;
+        if (saved) {
+            tabIcon.setIcon(savedTabIcon);
+        }
+        else {
+            tabIcon.setIcon(modifiedTabIcon);
+        }
+        parent.updateGui();
     }
 
     public boolean isFresh()
@@ -74,31 +109,144 @@ implements ChangeListener, ActionListener
         return file;
     }
 
+    public JTextArea getArea()
+    {
+        return area;
+    }
+
     public void setFile(File file)
     {
+        File last = this.file;
         this.file = file;
+        if (load()) {
+            tabLabel.setText(file.getName());
+            setSaved(true);
+        } else {
+            this.file = last;
+            load();
+        }
     }
 
-    private void load() {
-        // read file contents to the text area
+    public void saveToFile(File file)
+    {
+        System.out.println("saveToFile(): " + file);
+        boolean write = true;
+        if (file.exists()) {
+            JOptionPane confirm = new JOptionPane("The file already exists. Do you wish to overwrite it?",
+                                                  JOptionPane.QUESTION_MESSAGE,
+                                                  JOptionPane.YES_NO_OPTION,
+                                                  UIManager.getIcon("OptionPane.questionIcon"));
+            JDialog dialog = confirm.createDialog("Overwrite File");
+            dialog.setVisible(true);
+            Object selectedValue = confirm.getValue();
+            if (selectedValue != null) {
+                int value = (Integer)selectedValue;
+                if (value != JOptionPane.YES_OPTION) {
+                    write = false;
+                }
+            }
+        }
+        if (write) {
+            File last = this.file;
+            this.file = file;
+            if (store()) {
+                tabLabel.setText(file.getName());
+                setSaved(true);
+            } else {
+                this.file = last;
+            }
+        }
     }
 
-    private void store() {
-        // write text area contents to the file
+    public void save() {
+        if (store()) {
+            setSaved(true);
+        }
+    }
+
+    private boolean load() {
+        boolean result = true;
+        try {
+            StringBuffer contents = new StringBuffer();
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            char[] buffer = new char[4096];
+            int numRead = 0;
+
+            while ((numRead = reader.read(buffer)) != -1) {
+                String data = String.valueOf(buffer, 0, numRead);
+                contents.append(data);
+                buffer = new char[4096];
+            }
+            reader.close();
+
+            area.setText(contents.toString());
+        } catch (Exception e) {
+            result = false;
+        }
+        return result;
+    }
+
+    private boolean store() {
+        boolean result = true;
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write(area.getText());
+            writer.close();
+        } catch (Exception e) {
+            result = false;
+        }
+        return true;
+    }
+
+    public void close() {
+        boolean remove = true;
+        if (!isSaved()) {
+            JOptionPane confirm = new JOptionPane("This tab's contents have been modified. Do you wish to close without saving?",
+                                                  JOptionPane.QUESTION_MESSAGE,
+                                                  JOptionPane.YES_NO_OPTION,
+                                                  UIManager.getIcon("OptionPane.questionIcon"));
+            JDialog dialog = confirm.createDialog("Tab Contents Modified");
+            dialog.setVisible(true);
+            Object selectedValue = confirm.getValue();
+            if (selectedValue != null) {
+                int value = (Integer)selectedValue;
+                if (value != JOptionPane.YES_OPTION) {
+                    remove = false;
+                }
+            }
+        }
+        if (remove) {
+            pane.removeTabAt(pane.indexOfComponent(this));
+        }
     }
 
  // Delegate methods
     public void stateChanged(ChangeEvent evt) {
         Object source = evt.getSource();
+        parent.updateGui();
     }
 
     public void actionPerformed(ActionEvent evt) {
         Object source = evt.getSource();
-        if (source == closeTabButton) {
-            if (!isSaved()) {
-                // promt: "File has been modified, do you wish to close without saving?"
-            }
-            parent.removeTabAt(parent.indexOfComponent(this));
+        if (source == tabButton) {
+            close();
         }
+        parent.updateGui();
+    }
+
+    public void insertUpdate(DocumentEvent evt) {
+        setSaved(false);
+    }
+
+    public void removeUpdate(DocumentEvent evt) {
+        setSaved(false);
+    }
+
+    public void changedUpdate(DocumentEvent evt) {
+        setSaved(false);
+    }
+
+    public void caretUpdate(CaretEvent evt) {
+        parent.updateGui();
     }
 }
