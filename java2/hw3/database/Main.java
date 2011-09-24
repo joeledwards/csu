eaa5dee3c7d13eb7dd471cff9977691712bedce9
Joel Edwards <joeledwards@gmail.com>
2011-09-24 00:36:24 -0600

    Started work on Java2 Homework3

---
+++ java2/hw3/database/Main.java
@@ -0,0 +1,18 @@
+/*
+1) connect to a database
+2) generate a table
+3) populate the table
+4) select and display all rows
+5) update the table
+6) select and verify all updated rows
+7) generate second table & add data
+8) use join across tables
+*/
+
+public class Main
+{
+    public static void main(String argv[])
+    {
+    }
+}
+
