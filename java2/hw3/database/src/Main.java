cba4c9dfe6e07af01e084d7a33a7722e5e961f87
Joel Edwards <joeledwards@gmail.com>
2011-10-02 11:30:15 -0600

    Adjusted sample data and added comments

--- java2/hw3/database/src/Main.java
+++ java2/hw3/database/src/Main.java
@@ -1,13 +1,3 @@
-/*
-1) connect to a database
-2) generate a table
-3) populate the table
-4) select and display all rows
-5) update the table
-6) select and verify all updated rows
-7) generate second table & add data
-8) use join across tables
-*/
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
@@ -22,10 +12,11 @@ import java.util.Set;
 
 public class Main
 {
+    // Information for the database in a raw format.
     public static final String contacts[] = {
-        "Rev.,Joel,David,Edwards,,555.918.2106,Mobile,555.853.2582,Work,555.823.0449,Home",
-        ",Paul,,Paulson,,555.471.2208,Work,555.143.3444,Mobile",
-        ",Charles,Robert,Hutt,Ph.D,555.226.2818,Work,555.853.2266,Mobile"
+        "Rev.,Joel,David,Edwards,,555.728.2556,Mobile,555.873.2912,Work,555.823.0449,Home",
+        ",Robert,,Paulson,,555.471.2208,Work,555.143.3444,Mobile",
+        ",Charles,Daniel,Xavier,Ph.D,555.226.2818,Work,555.853.2266,Mobile"
     };
 
     public static void main(String argv[])
@@ -36,7 +27,7 @@ public class Main
         ResultSet ids = null;
         String value = null;
 
-        // This will load the MySQL driver, each DB has its own driver
+        // Load the MySQL JDBC driver
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException ex) {
@@ -44,6 +35,7 @@ public class Main
             System.exit(1);
         }
 
+        // Print a list of the available drivers
         System.out.printf("Available Drivers:\n");
         Enumeration<Driver> drivers = DriverManager.getDrivers();
         Driver d = null;
@@ -54,13 +46,15 @@ public class Main
         System.out.print("\n");
 
         try {
+            // Establish a connection to the database
             System.out.print("1.a) Connecting to the database...");
             conn = DriverManager.getConnection(
                    "jdbc:mysql://localhost/test?" + 
                    "user=test&password=test");
             System.out.print("Done.\n");
 
-
+            // Get rid of the tables from the last run
+            // but only if they exist
             System.out.print("1.b) Removing any test tables...");
             stmt = conn.prepareStatement(
                 "DROP TABLE IF EXISTS Contact, Phone;");
@@ -68,6 +62,7 @@ public class Main
             System.out.print("Done.\n");
 
 
+            // Create the fist table
             System.out.print("2)   Creating the first table (Contact)...");
             stmt = conn.prepareStatement(
                 "CREATE TABLE IF NOT EXISTS Contact (" + 
@@ -83,26 +78,39 @@ public class Main
             System.out.print("Done.\n");
 
 
+            // Popuate the first table with data
             System.out.print("3)   Populating the first table (Contact)...");
             HashMap<Integer,HashMap<String,String>> numbers;
             numbers = new HashMap<Integer,HashMap<String,String>>();
+
+            // Generate a prepared statement which can be re-used
+            // for each insert operation.
+            stmt = conn.prepareStatement(
+                "INSERT " + 
+                "INTO Contact(prefix, first, middle, last, suffix) " +
+                "VALUES (?, ?, ?, ?, ?)",
+                Statement.RETURN_GENERATED_KEYS);
+
+            // Loop through the raw data, extract the person's name
+            // and their phone numbers
             for (String info: contacts) {
+                // Ensure that we have sufficient data to 
+                // populate the person's name
                 String[] parts = info.split(",");
                 if (parts.length < 5) {
                     continue;
                 }
 
+                // Track the number of values processed for this contact
                 int processed = 0;
-                stmt = conn.prepareStatement(
-                    "INSERT " + 
-                    "INTO Contact(prefix, first, middle, last, suffix) " +
-                    "VALUES (?, ?, ?, ?, ?)",
-                    Statement.RETURN_GENERATED_KEYS);
+                // Add individual column values to the prepared statement
                 for (int i = 0; i < 5; i++) {
                     value = parts[i].trim();
                     stmt.setString(i+1, value);
                 }
                 stmt.executeUpdate();
+
+                // Extract the id for the inserted row
                 ids = stmt.getGeneratedKeys();
                 ids.next();
                 int id = ids.getInt(1);
@@ -126,6 +134,8 @@ public class Main
             System.out.print("Done.\n");
 
 
+            // Dump the contents of the fist table to verify
+            // that it was correctly created and populated
             System.out.printf("4)   Checking the table's contents:\n");
             stmt = conn.prepareStatement("SELECT * FROM Contact;");
             results = stmt.executeQuery();
@@ -133,6 +143,10 @@ public class Main
             while (results.next()) {
                 System.out.print("      >");
                 for (int i = 1; i < 20; i++) {
+                    // For a simple inplementation such as this we can
+                    // simply check up to 20 columns, and try to extract
+                    // the data in first string then integer format,
+                    // stopping once neither works.
                     try {
                         value = results.getString(i);
                         if (value.length() > 0) {
@@ -152,6 +166,8 @@ public class Main
             }
 
 
+            // Update the first table with an additional
+            // person's information
             System.out.print("5)   Updating the first table (Contact)...");
             stmt = conn.prepareStatement(
                 "INSERT IGNORE " +
@@ -167,6 +183,8 @@ public class Main
             System.out.print("Done.\n");
 
 
+            // Dump the contents of the fist table to verify
+            // that it was correctly updated
             System.out.printf("6)   Checking the table's contents:\n");
             stmt = conn.prepareStatement("SELECT * FROM Contact;");
             results = stmt.executeQuery();
@@ -193,6 +211,7 @@ public class Main
             }
 
 
+            // Create the second table
             System.out.print("7.a) Creating the second table (Phone)...");
             stmt = conn.prepareStatement(
                 "CREATE TABLE IF NOT EXISTS Phone (" + 
@@ -203,16 +222,21 @@ public class Main
             stmt.execute();
             System.out.print("Done.\n");
 
+            // Generate a prepared statement which can be re-used
+            // for each insert operation
+            stmt = conn.prepareStatement(
+                "INSERT IGNORE " +
+                "INTO Phone(number, description, contact_id) " +
+                "VALUES (?, ?, ?)");
+
+            // Insert values into the second table
             System.out.print("7.b) Populating the second table (Phone)...");
             Set<Integer> keys = numbers.keySet();
             for (Integer id: keys) {
                 HashMap<String,String> numList = numbers.get(id);
                 Set<String> types = numList.keySet();
+                // Add individual column values to the prepared statement
                 for (String number: types) {
-                    stmt = conn.prepareStatement(
-                        "INSERT IGNORE " +
-                        "INTO Phone(number, description, contact_id) " +
-                        "VALUES (?, ?, ?)");
                     stmt.setString(1, number);
                     stmt.setString(2, numList.get(number));
                     stmt.setInt(3, id);
@@ -221,6 +245,8 @@ public class Main
             }
             System.out.print("Done.\n");
 
+            // Dump a join of both tables to confirm that all data
+            // was added, and they were correctly tied together.
             System.out.print("8)   Verifying all contact info (join):\n");
             stmt = conn.prepareStatement(
                 "SELECT * " +
