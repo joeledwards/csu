b7c0fd46e20840326d57211f8ee2bfc62533fa2c
Joel Edwards <joeledwards@gmail.com>
2011-11-26 14:24:30 -0700

    Removed another unnecesary file

--- java2/hw9/print/src/Resources.java
+++
@@ -1,9 +0,0 @@
-import java.awt.Image;
-import javax.swing.ImageIcon;                                                                  
-
-public class Resources {
-    public static ImageIcon getAsImageIcon(String name, int height, int width) {
-        return new ImageIcon((new ImageIcon(ClassLoader.getSystemResource(name))).getImage().getScaledInstance(height, width, Image.SCALE_SMOOTH));
-    }
-}
-
