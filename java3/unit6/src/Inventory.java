a94cfb5acb92bbb68bd3f2fa9ea4097e388ca1d3
Joel Edwards <joeledwards@gmail.com>
2012-05-17 02:33:57 -0600

    Updates to Java3 Unit6

---
+++ java3/unit6/src/Inventory.java
@@ -0,0 +1,52 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+import inventory.BookT;
+import inventory.InventoryT;
+import inventory.ObjectFactory;
+
+public class Inventory {
+
+    private ObjectFactory factory;
+    private InventoryT inventory;
+
+    public Inventory() {
+        factory = new ObjectFactory();
+        inventory = factory.createInventoryT();
+    }
+
+    public void addBook(String      title,
+                        String      author,
+                        String      publisher,
+                        BigInteger  year,
+                        BigInteger  isbn,
+                        double      price)
+    {
+        BookT book = factory.createBookT();
+        book.setTitle(title);
+        book.setAuthor(author);
+        book.setPublisher(publisher);
+        book.setYear(year);
+        book.setIsbn(isbn);
+        book.setPrice(price);
+
+        ArrayList<BookT> books = (ArrayList)(inventory.getBook());
+        books.add(book);
+    }
+
+    public void marshal() {
+        try {
+            JAXBElement<InventoryT> inv = factory.createInventory( inventory );
+            JAXBContext context = JAXBContext.newInstance( "hello" );
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.marshal( inv, System.out );
+        } catch( JAXBException ex ){
+            // ...
+        }
+    }
+}
